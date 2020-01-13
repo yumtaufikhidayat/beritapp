898baea34de3e5f32fb317a8b7d17e862e905896
yumtaufikhidayat <taufik.hidayat@merahputih.id>
2020-01-13 11:50:37 +0700

    Adding adapter to project

---
+++ app/src/main/java/com/example/beritapp/adapter/Adapter.java
@@ -0,0 +1,92 @@
+package com.example.beritapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.request.RequestOptions;
+import com.example.beritapp.R;
+import com.example.beritapp.model.Article;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    private List<Article> articles;
+    private Context context;
+    private OnItemClickListener onItemClickListener;
+
+    public Adapter(List<Article> articles, Context context) {
+        this.articles = articles;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+        return new MyViewHolder(view, onItemClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holders, int position) {
+
+        final MyViewHolder holder = holders;
+        Article model = articles.get(position);
+
+        RequestOptions requestOptions = new RequestOptions();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(View view, int position);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView title, desc, author, publishedAt, source, time;
+        ImageView imageView;
+        ProgressBar progressBar;
+        OnItemClickListener onItemClickListener;
+
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+            desc = itemView.findViewById(R.id.desc);
+            author = itemView.findViewById(R.id.author);
+            publishedAt = itemView.findViewById(R.id.publishedAt);
+            source= itemView.findViewById(R.id.source);
+            time = itemView.findViewById(R.id.time);
+            imageView = itemView.findViewById(R.id.img);
+            progressBar = itemView.findViewById(R.id.progress_load_photo);
+
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            onItemClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+}
