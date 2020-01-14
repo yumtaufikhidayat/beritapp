090b7554724ee4d5bfbac04edc53ea366cab976c
yumtaufikhidayat <taufik.hidayat@merahputih.id>
2020-01-14 10:40:16 +0700

    Adding news detail layout

---
+++ app/src/main/java/com/example/beritapp/activity/NewsDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.beritapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.beritapp.R;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
